3bfcfe1b5d95df63f3dc32ddb9e3e1cc21d4a41b
Norman Cantoral <toliveanddie76@gmail.com>
2017-05-05 22:19:02 +0000

    add random cards to index page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,8 +2,9 @@ module WelcomeHelper
     def show_random_card(cards)
         a = nil
         while (a == nil) do
-            a = cards.shuffle.pop.img
+            b = cards.shuffle.pop
+            a = b.img
         end
-        return a
+        return b
     end
 end
