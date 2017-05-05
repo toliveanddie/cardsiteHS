76ad04a2cb12c490baacd98c020e51c8ab52eee6
Norman Cantoral <toliveanddie76@gmail.com>
2017-05-05 21:31:10 +0000

    update index page with helper method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @cards = Card.all
   end
   
   def druid
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+    def show_random_card(cards)
+        a = nil
+        while (a == nil) do
+            a = cards.shuffle.pop.img
+        end
+        return a
+    end
 end
