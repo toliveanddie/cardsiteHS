10351763d08a6829fb3705051aff8bd21a85b76e
Norman Cantoral <toliveanddie76@gmail.com>
2017-05-06 00:56:12 +0000

    make index random cards only include collectible cards with an image attribute

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,9 +1,13 @@
 module WelcomeHelper
     def show_random_card(cards)
-        a = nil
-        while (a == nil) do
-            b = cards.shuffle.pop
-            a = b.img
+        check = nil
+        collect = false
+        while (check == nil) do
+            while (collect == false) do
+                b = cards.shuffle.pop
+                collect = b.collectible?
+            end
+            check = b.img
         end
         return b
     end
