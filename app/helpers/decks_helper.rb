4252be3bc06b0a5ccffc06ebbb97d79d9e12f94a
Norman Cantoral <toliveanddie76@gmail.com>
2017-05-08 00:16:28 +0000

    fixing client side issues

--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,11 +1,13 @@
 module DecksHelper
     
     def arena_random_card
-        
-        cards = Card.all
-        random_card = cards.shuffle.pop
+        check_hero = "hero"
+        while (check_hero == "hero") do
+            cards = Card.all
+            random_card = cards.shuffle.pop
+            check_hero = random_card
+        end
         return random_card
-        
     end
 
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,8 +1,12 @@
 module WelcomeHelper
     
     def show_random_card
-        cards = Card.all
-        random_card = cards.shuffle.pop
+        hero_check = "hero"
+        while (hero_check == "hero") do
+            cards = Card.all
+            random_card = cards.shuffle.pop
+            hero_check = random_card
+        end
         return random_card
     end
     
