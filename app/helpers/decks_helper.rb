d3e9fb335370838e2d161a062983c412b0f47d1a
Norman Cantoral <toliveanddie76@gmail.com>
2017-05-08 18:01:07 +0000

    add method to show a particular rarity in arena

--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,10 +1,50 @@
 module DecksHelper
     
-    def arena_random_card
+    def three_random_heroes
+        heroes = ["Anduin Wrynn",
+                  "Garrosh Hellscream",
+                  "Gul'dan", 
+                  "Jaina Proudmoore", 
+                  "Malfurion Stormrage", 
+                  "Rexxar", 
+                  "Thrall",
+                  "Uther Lightbringer",
+                  "Valeera Sanguinar"]
+        return heroes.shuffle.pop(3)
+    end
+    
+    def random_rarity
+    
+        chance_array = []
+        2.times do
+            chance_array.push("Legendary")
+        end
+        5.times do
+            chance_array.push("Epic")
+        end
+        20.times do
+            chance_array.push("Rare")
+        end
+        80.times do
+            chance_array.push("Common")
+        end
+        80.times do
+            chance_array.push("Free")
+        end
+        return chance_array.shuffle.pop
+    end
+        
+    def arena_random_card(card_rarity)
+        cards = Card.all
+        rarity_cards = []
         check_hero = "Hero"
+        cards.each do |card|
+            if card.rarity == card_rarity
+                rarity_cards << card
+            end
+        end
         while (check_hero == "Hero") do
-            cards = Card.all
-            random_card = cards.shuffle.pop
+            random_card = rarity_cards.shuffle.pop
             check_hero = random_card.cardType
         end
         return random_card
